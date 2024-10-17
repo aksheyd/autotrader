66eab17817479fc3fc084b10072c1ae7b0a918f1
arsaln03 <112825290+arsaln03@users.noreply.github.com>
2024-10-17 18:58:54 -0400

    formatted api output + testing vim stuff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+import json
 
-IBM_data_fetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XD63JNDBUQX3NEJ'
-r = requests.get(IBM_data_fetch)
-data = r.json()
+IBM_Data_Fetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XD63JNDBUQX3NEJ'
+response = requests.get(IBM_Data_Fetch)
+data = response.json()
 
-print(data)
+print(json.dumps(data, indent = 4))
+# print(data.keys())
+# print(data["Meta Data"])
+# print(data["Time Series (5min)"])
