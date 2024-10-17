6d40ca5de79a33f9f41111c143eb96c30e915d93
arsaln03 <112825290+arsaln03@users.noreply.github.com>
2024-10-17 18:02:48 -0400

    renamed variables for clarity

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XD63JNDBUQX3NEJ'
-r = requests.get(url)
+IBM_data_fetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XD63JNDBUQX3NEJ'
+r = requests.get(IBM_data_fetch)
 data = r.json()
 
 print(data)
-
