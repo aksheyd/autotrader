af2928e22a3db0cc2d46c3e714361ea6d7f993cd
Akshey D <aksheyd@umich.edu>
2024-10-18 11:36:58 -0400

    ready for you to work on

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import requests
+"""Make trades automatically using Nancy Pelosi's stock trades"""
 import json
+import requests
 
-IBM_Data_Fetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9XD63JNDBUQX3NEJ'
-response = requests.get(IBM_Data_Fetch)
-data = response.json()
+with open('config.json', encoding='utf-8') as f:
+    api_key = json.load(f)['API_KEY']
 
-print(json.dumps(data, indent = 4))
-# print(data.keys())
-# print(data["Meta Data"])
-# print(data["Time Series (5min)"])
+def create_url(function, symbol):
+    """Create a URL for the Alpha Vantage API"""
+    return f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={api_key}'
+
+def get_time_series_daily(symbol):
+    """Get the daily time series for a stock"""
+    url = create_url('TIME_SERIES_DAILY', symbol)
+    response = requests.get(url, timeout=5)
+    return response.json()
+
+def get_time_series_intraday(symbol):
+    """Get the intraday time series for a stock"""
+    url = create_url('TIME_SERIES_INTRADAY', symbol)
+    response = requests.get(url, timeout=5)
+    return response.json()
+
+
+def main():
+    """Main"""
+    symbol = 'AAPL'
+    time_series = get_time_series_daily(symbol)
+    print("Apple stock high on 2024-10-17:",
+          time_series['Time Series (Daily)']['2024-10-17']['2. high'])
+
+if __name__ == '__main__':
+    main()
