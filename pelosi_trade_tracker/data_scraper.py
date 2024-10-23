a0643c16acbf80d99fe3f0d654b42a4ab5c85943
arsaln03 <112825290+arsaln03@users.noreply.github.com>
2024-10-23 19:34:24 -0400

    code now fully accounts for pelosi trades including stock, buy/sell/exchange, approximate value, and date

--- pelosi_trade_tracker/data_scraper.py
+++ pelosi_trade_tracker/data_scraper.py
@@ -13,19 +13,19 @@ except requests.Timeout:
 soup = BeautifulSoup(r.content, 'html.parser')
 rows = soup.select('tbody tr')
 
-# print(rows)
-
 for row in rows:
     ticker = row.find('a', class_ = 'text-txt-interactive')
     date = row.find('div', class_ = 'text-size-3 font-medium')
     bought = row.find('span', class_ = 'tx-type--buy')
     sold = row.find('span', class_ = 'tx-type--sell')
     exchange = row.find('span', class_ = 'tx-type--exchange')
+    value = row.find('span', class_ = 'mt-1')
 
     if bought:
         print(
             "Stock: " + ticker.get_text(strip = True) + "\n" +
             "Order Type: Buy" + "\n" + 
+            "Value: $" + value.get_text(strip = True) + "\n" + 
             "Date: " + date.get_text(strip = True),
             "\n"
         )
@@ -33,6 +33,7 @@ for row in rows:
         print(
             "Stock: " + ticker.get_text(strip = True) + "\n" +
             "Order Type: Sell" + "\n" + 
+            "Value: $" + value.get_text(strip = True) + "\n" + 
             "Date: " + date.get_text(strip = True),
             "\n"
         )
@@ -40,6 +41,7 @@ for row in rows:
         print(
             "Stock: " + ticker.get_text(strip = True) + "\n" +
             "Order Type: Exchange" + "\n" + 
+            "Value: $" + value.get_text(strip = True) + "\n" + 
             "Date: " + date.get_text(strip = True),
             "\n"
         )
