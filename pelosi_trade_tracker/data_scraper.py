e03603bbbeabb2f47e4abe09e66033ae6594465a
arsaln03 <112825290+arsaln03@users.noreply.github.com>
2024-10-23 19:22:42 -0400

    made formatting better IMO

--- pelosi_trade_tracker/data_scraper.py
+++ pelosi_trade_tracker/data_scraper.py
@@ -14,18 +14,22 @@ soup = BeautifulSoup(r.content, 'html.parser')
 rows = soup.select('tbody tr')
 
 for row in rows:
-    ticker_header = row.find('a', class_ = 'text-txt-interactive')
+    ticker = row.find('a', class_ = 'text-txt-interactive')
     date = row.find('div', class_ = 'text-size-3 font-medium')
     bought = row.find('span', class_ = 'tx-type--buy')
     sold = row.find('span', class_ = 'tx-type--sell')
 
     if bought:
         print(
-            "Stock " + ticker_header.get_text(strip = True) + 
-            " BOUGHT on: " + date.get_text(strip = True)
+            "Stock: " + ticker.get_text(strip = True) + "\n" +
+            "Order Type: Buy" + "\n" + 
+            "Date: " + date.get_text(strip = True),
+            "\n"
         )
     elif sold:
         print(
-            "Stock " + ticker_header.get_text(strip = True) + 
-            " SOLD on: " + date.get_text(strip = True)
+            "Stock: " + ticker.get_text(strip = True) + "\n" +
+            "Order Type: Sell" + "\n" + 
+            "Date: " + date.get_text(strip = True),
+            "\n"
         )
