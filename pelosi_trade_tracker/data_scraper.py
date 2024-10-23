4f836add4874f4b0d5ebb6b10d56fb7f957285a5
Akshey D <aksheyd@umich.edu>
2024-10-23 15:53:36 -0400

    arsal updates

--- pelosi_trade_tracker/data_scraper.py
+++ pelosi_trade_tracker/data_scraper.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL  = "https://www.capitoltrades.com/politicians/P000197"
+URL = "https://www.capitoltrades.com/politicians/P000197"
 
 try:
     r = requests.get(URL, timeout = 10)
@@ -14,5 +14,9 @@ soup = BeautifulSoup(r.content, 'html.parser')
 rows = soup.select('tbody tr')
 
 for row in rows:
-    ticker_header = row.find('a', class_='text-txt-interactive')
-    print(ticker_header.get_text(strip = True))
+    ticker_header = row.find('a', class_ = 'text-txt-interactive')
+    date = row.find('div', class_='text-size-3 font-medium')
+    print(
+        "Stock " + ticker_header.get_text(strip = True) + 
+        " bought on : " + date.get_text(strip = True)
+    )
