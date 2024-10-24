d18da1748e18f03844fa6754e4dc8d6b58f1b29f
arsaln03 <112825290+arsaln03@users.noreply.github.com>
2024-10-24 16:38:31 -0400

    formatted code to make it readable

--- pelosi_trade_tracker/data_scraper.py
+++ pelosi_trade_tracker/data_scraper.py
@@ -21,6 +21,45 @@ class DataScraper:
         new_url = url.split('page=')[0] + f"page={page_num + 1}"
         return new_url
 
+    def process_rows(self, rows):
+        """finds and appends useful data for each trade to trade list"""
+        for row in rows:
+            stock_name = row.find('a', class_ = 'text-txt-interactive')
+            ticker = row.find('span', class_ = 'q-field')
+            value = row.find('span', class_ = 'mt-1')
+            trade_date = row.find_all('div', class_ = 'text-size-3 font-medium')[1]
+            trade_year = row.find_all('div', class_ = 'text-size-2 text-txt-dimmer')[1]
+            report_date = row.find('div', class_ = 'text-size-3 font-medium')
+            report_year = row.find('div', class_ = 'text-size-2 text-txt-dimmer')
+
+            order_type = None
+            if row.find('span', class_ = 'tx-type--buy'):
+                order_type = "Buy"
+            elif row.find('span', class_ = 'tx-type--sell'):
+                order_type = "Sell"
+            elif row.find('span', class_ = 'tx-type--exchange'):
+                order_type = "Exchange"
+            elif row.find('span', class_ = "tx-type--receive"):
+                order_type = "Receive"
+            else:
+                print("Unknown Order Type, request failed!")
+                return
+
+            self.trade_data.append({
+                "stock_name": stock_name.get_text(strip=True) if stock_name else "N/A",
+                "ticker": ticker.get_text(strip=True) if ticker else "N/A",
+                "order_type": order_type,
+                "value": value.get_text(strip=True) if value else "N/A",
+                "trade_date": (
+                    f"{trade_date.get_text(strip=True)} "
+                    f"{trade_year.get_text(strip=True)}"
+                ),
+                "report_date": (
+                    f"{report_date.get_text(strip=True)} "
+                    f"{report_year.get_text(strip=True)}"
+                )
+            })
+
     def parse_url(self, url):
         """takes in URL input, stores stock trade data in trade_data"""
         try:
@@ -52,42 +91,7 @@ class DataScraper:
             r = requests.get(self.increment_url(url, first_page), timeout = 5)
             soup = BeautifulSoup(r.content, 'html.parser')
             rows = soup.select('tbody tr')
-            for row in rows:
-                stock_name = row.find('a', class_ = 'text-txt-interactive')
-                ticker = row.find('span', class_ = 'q-field')
-                value = row.find('span', class_ = 'mt-1')
-                trade_date = row.find_all('div', class_ = 'text-size-3 font-medium')[1]
-                trade_year = row.find_all('div', class_ = 'text-size-2 text-txt-dimmer')[1]
-                report_date = row.find('div', class_ = 'text-size-3 font-medium')
-                report_year = row.find('div', class_ = 'text-size-2 text-txt-dimmer')
-
-                order_type = None
-                if row.find('span', class_ = 'tx-type--buy'):
-                    order_type = "Buy"
-                elif row.find('span', class_ = 'tx-type--sell'):
-                    order_type = "Sell"
-                elif row.find('span', class_ = 'tx-type--exchange'):
-                    order_type = "Exchange"
-                elif row.find('span', class_ = "tx-type--receive"):
-                    order_type = "Receive"
-                else:
-                    print("Unknown Order Type, request failed!")
-                    return
-
-                self.trade_data.append({
-                    "stock_name": stock_name.get_text(strip=True) if stock_name else "N/A",
-                    "ticker": ticker.get_text(strip=True) if ticker else "N/A",
-                    "order_type": order_type,
-                    "value": value.get_text(strip=True) if value else "N/A",
-                    "trade_date": (
-                        f"{trade_date.get_text(strip=True)} "
-                        f"{trade_year.get_text(strip=True)}"
-                    ),
-                    "report_date": (
-                        f"{report_date.get_text(strip=True)} "
-                        f"{report_year.get_text(strip=True)}"
-                    )
-                })
+            self.process_rows(rows)
             first_page += 1
 
     def print_trade_data(self):
@@ -110,17 +114,20 @@ class DataScraper:
 
 def main():
     """Main"""
-    # pelosi trades URL = https://www.capitoltrades.com/politicians/P000197
+    # pelosi trades URL = https://www.capitoltrades.com/politicians/P000197?page=1
     get_pelosi_trades = DataScraper()
     get_pelosi_trades.want_politician_name(True)
     get_pelosi_trades.parse_url("https://www.capitoltrades.com/politicians/P000197?page=1")
     get_pelosi_trades.print_trade_data()
 
-    # mcconnell trades URL= https://www.capitoltrades.com/politicians/M000355
+    # mcconnell trades URL= https://www.capitoltrades.com/politicians/M000355?page=1
     # get_mcconnell_trades = DataScraper()
     # get_mcconnell_trades.want_politician_name(True)
-    # get_mcconnell_trades.parse_url("https://www.capitoltrades.com/politicians/M000355")
+    # get_mcconnell_trades.parse_url("https://www.capitoltrades.com/politicians/M000355?page=1")
     # get_mcconnell_trades.print_trade_data()
+
+    # mccaul trades URL = https://www.capitoltrades.com/politicians/M001157?page=1
+    # 400 PAGES OF TRADES -- DO NOT RUN YET!!!
 
 if __name__ == "__main__":
     main()
